8e41b7f310568cd0a0409f255aa95c55a975b186
khairul111010 <khairul18103199@gmail.com>
2022-02-13 01:44:15 +0600

    test

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	cout << "test";
+	return 0;
+}
